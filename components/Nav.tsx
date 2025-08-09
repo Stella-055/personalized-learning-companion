a02a3a83a44a0e49a876b013e10c9483043e0331
Stella-055 <stellaongayo243@gmail.com>
2025-08-09 11:41:17 +0300

    added navbar

---
+++ components/Nav.tsx
@@ -0,0 +1,45 @@
+"use client"
+import Image from 'next/image';
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {cn} from "@/lib/utils";
+const navItems = [
+    { label:'Home', href: '/' },
+    { label: 'Companions', href: '/companions' },
+    { label: 'My Journey', href: 'my-journey' },
+]
+
+const Nav = () => {
+    const pathname = usePathname();
+  return (
+    <nav className='flex justify-around py-2'>
+    <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image
+                        src="/images/logo.svg"
+                        alt="logo"
+                        width={46}
+                        height={44}
+                    />
+                </div>
+            </Link>
+            <div className="flex items-center gap-8">
+            <nav className="flex items-center gap-4">
+            {navItems.map(({ label, href }) => (
+                <Link
+                    href={href}
+                    key={label}
+                    className={cn(pathname === href && 'text-orange-500 font-semibold')}
+                >
+                    {label}
+                </Link>
+            ))}
+        </nav>
+         
+                
+            </div>
+    </nav>
+  )
+}
+
+export default Nav
